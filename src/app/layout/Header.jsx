562ef7d18fde607af86517c499a6809d19bd6433
Akovi Mensah <akovi66@gmail.com>
2022-08-07 13:10:23 -0500

    styled appBar

--- src/app/layout/Header.jsx
+++ src/app/layout/Header.jsx
@@ -1,28 +1,78 @@
+import { ShoppingCart } from '@mui/icons-material'
+import { AppBar, Badge, Box, IconButton, List, ListItem, Toolbar, Typography } from '@mui/material'
 import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
 import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+
+const midLinks = [
+    { title: 'pizzas', path: '/' },
+    { title: 'about', path: '/about' },
+    { title: 'contact', path: '/contact' }
+]
+
+const rightLinks = [
+    { title: 'login', path: '/login' },
+    { title: 'register', path: '/register' }
+]
+
+const navStyles = {
+    color: 'inherit',
+    textDecoration: 'none',
+    typography: 'h6',
+    '&:hover': {
+        color: 'grey.500'
+    },
+    '&.active': {
+        color: 'text.secondary'
+    }
+}
 
 const Header = () => {
-    const { basket } = useSelector(state => state.basket)
+    const { basket } = useSelector(state => state.basket);
     const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0)
+
     return (
-        <header>
-            <Navbar bg="primary" variant="dark">
-                <Container>
-                    <Navbar.Brand href="/">PizzaBox</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Link className='pe-1' to="/"><i className='fas'>Home</i></Link>
-                        <Link className='pe-1' to="/contact"><i className='fas'>Contact</i></Link>
-                    </Nav>
-                    <Nav className="ml-auto">
-                        <Link className='pe-1' to='/basket'><i className='fas fa-shopping-cart'> Cart ({itemCount}) </i> </Link>
-                        <Link className='pe-1' to="/login"><i className='fas fa-user'> Login </i> </Link>
-                        <Link to="/register"><i className='fas fa-user'> Register</i></Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-        </header >
+        <AppBar position='static' sx={{ mb: 4 }}>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Box display='flex' alignItems='center'>
+                    <Typography variant='h6' component={NavLink} exact to='/'
+                        sx={navStyles}>
+                        PizzaBox
+                    </Typography>
+                </Box>
+                <List sx={{ display: 'flex' }}>
+                    {midLinks.map(({ title, path }) => (
+                        <ListItem
+                            component={NavLink}
+                            to={path}
+                            key={path}
+                            sx={navStyles}
+                        >
+                            {title.toUpperCase()}
+                        </ListItem>
+                    ))}
+                </List>
+                <Box display='flex' alignItems='center'>
+                    <IconButton component={Link} to='/basket' size='large' sx={{ color: 'inherit' }}>
+                        <Badge badgeContent={itemCount} color='secondary'>
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+                    <List sx={{ display: 'flex' }}>
+                        {rightLinks.map(({ title, path }) => (
+                            <ListItem
+                                component={NavLink}
+                                to={path}
+                                key={path}
+                                sx={navStyles}
+                            >
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+            </Toolbar>
+        </AppBar>
     )
 }
 
