d65c259a312a758cd375e3a3321cb71c7123d8e5
Akovi Mensah <akovi66@gmail.com>
2022-12-18 12:34:47 -0600

    Added option component

--- src/pages/custom-pizza/Crust.jsx
+++ src/pages/custom-pizza/Crust.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react'
+import { Col, Row } from 'react-bootstrap';
+import Option from './Option';
 
 const Crust = () => {
-    const [crusts, setCrusts] = useState(["Original", "Stuffed", "Widd"]);
+    const [crusts, setCrusts] = useState([{ name: "Original", image: "https://images.unsplash.com/photo-1573821663912-569905455b1c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" },
+    { name: "Stuffed", image: "https://images.unsplash.com/photo-1573821663912-569905455b1c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" },
+    { name: "Grilled", image: "https://images.unsplash.com/photo-1571407970349-bc81e7e96d47?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=725&q=80" }]);
     const [selected, setSelected] = useState(null);
 
     return (
-        crusts.map((crust, index) => (
-            <div key={index}><h1>{crust}</h1></div>
-        ))
+        <Row className='m-3' sm={12} md={6} lg={4} xl={4}>
+            {
+                crusts.map((crust) => (
+                    <Option key={crust.name} item={crust} />
+                ))
+            }
+        </Row>
+
     )
 }
 
---
+++ src/pages/custom-pizza/Option.jsx
@@ -0,0 +1,31 @@
+import { LoadingButton } from '@mui/lab'
+import { CardActions, CardContent, CardMedia } from '@mui/material'
+import { Button, Card } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { currencyFormat } from '../../app/util/util'
+import { addBasketItemAsync } from '../basket/basketSlice'
+
+const Option = ({ item }) => {
+    const { status } = useSelector(state => state.basket)
+    const dispatch = useDispatch();
+
+    return (
+        <Card className='m-1'>
+            <Link to={`/crust`}>
+                <CardMedia to={`/crust`}
+                    sx={{ height: 200, backgroundSize: 'flex', bgcolor: 'primary.light' }}
+                    image={item.image}
+                    title={item.name}
+                />
+            </Link>
+            <CardContent>
+                <h2>
+                    {item.name}
+                </h2>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Option
